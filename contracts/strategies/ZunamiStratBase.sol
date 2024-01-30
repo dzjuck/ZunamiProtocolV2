ad41e3aedca23feeb2b06599e56d337221ae1f6f
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2024-01-30 21:45:23 +0200

    Simplify oracle library name

--- contracts/strategies/ZunamiStratBase.sol
+++ contracts/strategies/ZunamiStratBase.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
-import '../lib/ConicOracle/interfaces/IOracle.sol';
+import '../lib/Oracle/interfaces/IOracle.sol';
 import '../interfaces/IStrategy.sol';
 import './ZunamiPoolAccessControl.sol';
 
